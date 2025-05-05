2998d9b4c9c280602f4ec9aaab817a76467663d9
MDH-mohawk <mohawk1999@hotmail.nl>
2025-05-05 12:39:47 +0200

    Made a working test for the reddit post component

--- src/Components/Reddit_post/Reddit_post.test.js
+++ src/Components/Reddit_post/Reddit_post.test.js
@@ -1,4 +1,66 @@
 import React from "react";
-import ReactDOM from 'react-dom/client'
 import "@testing-library/jest-dom";
 import RedditPost from "./Reddit_post";
+import { screen,fireEvent} from "@testing-library/react";
+import { render } from "@testing-library/react";
+
+describe("Reddit post component",() =>{
+
+    test("The like button gets filled",() => {
+
+        //Setup
+        render(<RedditPost img_src={undefined} description_text="its this" />);
+        const button = screen.getByTestId("Like_button");
+
+        //Exercise
+
+        fireEvent.click(button);
+
+        //Verify
+        expect(button.style.fill).toBe("#00C86B");
+
+    });
+
+    test("The like button gets a stroke",() => {
+
+        //Setup
+        render(<RedditPost img_src={undefined} description_text="its this" />);
+        const button = screen.getByTestId("Like_button");
+
+        //Exercise
+
+        fireEvent.click(button);
+
+        //Verify
+        expect(button.style.stroke).toBe("none");
+
+    });
+    
+    test("The dislike button gets filled",() => {
+
+        //Setup
+        render(<RedditPost img_src={undefined} description_text="its this" />);
+        const button = screen.getByTestId("Dislike_button");
+
+        //Exercise
+        fireEvent.click(button);
+
+        //Verify
+        expect(button.style.fill).toBe("#EF3535")
+    })
+
+    test("The dislike button gets a stroke",() => {
+
+        //Setup
+        render(<RedditPost img_src={undefined} description_text="its this" />);
+        const button = screen.getByTestId("Dislike_button");
+
+        //Exercise
+
+        fireEvent.click(button);
+
+        //Verify
+        expect(button.style.stroke).toBe("none");
+
+    });
+})

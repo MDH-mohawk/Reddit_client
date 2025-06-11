34b27e04c23125cba7599df0e805a8b838a95265
MDH-mohawk <mohawk1999@hotmail.nl>
2025-06-11 23:15:51 +0200

    Added test for Category button to empty search field on click

--- src/Components/Category/Category.test.js
+++ src/Components/Category/Category.test.js
@@ -4,13 +4,22 @@ import "@testing-library/jest-dom";
 import { screen,fireEvent, render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { configureStore, createSlice } from "@reduxjs/toolkit";
+import SearchBarHeader from "../Searchbar_header/SearchBarHeader";
 
 
 //mock reducer for testing purposes
 const mockReducer = createSlice({
     name:"category",
     initialState:{
-        category:"Apps"
+        category:"Apps",
+    },
+    reducers:{}
+})
+
+const serachMockReducer = createSlice({
+    name:"category",
+    initialState:{
+        searchTerm:"Pokemon"
     },
     reducers:{}
 })
@@ -18,23 +27,26 @@ const mockReducer = createSlice({
 // mock store added for testing purposes
 const mockStore = configureStore({
     reducer:{
-        categories:mockReducer.reducer
+        categories:mockReducer.reducer,
+        search:serachMockReducer.reducer
     }
 })
 
-
+beforeEach(()=>{
+    render(
+        <Provider store={mockStore}>
+        <MemoryRouter initialEntries={["/Apps"]}>
+            <Category cat={"Apps"}/>
+            <SearchBarHeader/>
+        </MemoryRouter>
+        </Provider>
+        );
+})
 
 describe("Category component",() => {
     test("New class is added to classlist of Category component button",() => {
 
         //Setup
-        render(
-        <Provider store={mockStore}>
-        <MemoryRouter initialEntries={["/Apps"]}>
-            <Category cat={"Apps"}/>
-        </MemoryRouter>
-        </Provider>
-        );
         const button = screen.getByTestId("cat_test")
 
         //Exercise
@@ -43,5 +55,18 @@ describe("Category component",() => {
         //Verify
         expect(button.children[0].classList[1]).toBe("categoryselected")
 
+    })
+
+    test("searh term is empty",() => {
+
+        //Setup
+        const search = screen.getByTestId("input_works")
+        const button = screen.getByTestId("cat_test")
+
+        //Exercise
+        fireEvent.click(button)
+        
+        //Verify
+        expect(search.value).toBe("")
     })
 })
